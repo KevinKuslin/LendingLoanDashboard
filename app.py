9a78f88495383b9db52842fa8125b7fe659407f2
KevinKuslin <tyrantnitrox@gmail.com>
2026-08-01 15:20:28 +0700

    Debug why healtchecks failed

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from dash import Dash, html, page_container
 import dash_bootstrap_components as dbc
+
+print("START APP")
+
 import callbacks.executive_callbacks
+print("EXECUTIVE CALLBACKS LOADED")
+
 import callbacks.clustering_callbacks
+print("CLUSTERING CALLBACKS LOADED")
+
 import callbacks.pattern_callbacks
+print("PATTERN CALLBACKS LOADED")
+
 import callbacks.anomaly_callbacks
+print("ANOMALY CALLBACKS LOADED")
+
 from components.navbar import navbar
-import os 
+print("NAVBAR LOADED")
+
+import os
 
 app = Dash(
 
